384eb0d827ab5b36baa8db69ce57bc4d1f1c2def
André Roaldseth <andre@roaldseth.net>
2018-09-24 15:21:28 +0200

    Set up as a standalone project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,40 @@
 plugins {
-    kotlin("jvm")
-    id("java-library")
-    id("com.commercehub.gradle.plugin.avro") version "0.14.2"
+    "java-library"
+    kotlin("jvm") version "1.2.51"
+    id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("com.palantir.docker") version "0.20.1"
+    id("com.palantir.git-version") version "0.11.0"
 }
 
-val avroVersion = "1.8.2"
+apply {
+    plugin("com.diffplug.gradle.spotless")
+}
+
+repositories {
+    jcenter()
+    maven(url = "http://packages.confluent.io/maven/")
+}
+
+version = "0.0.1"
+
+val kotlinLoggingVersion = "1.4.9"
 
 dependencies {
-    api("org.apache.avro:avro:$avroVersion")
+    implementation(kotlin("stdlib"))
+
+    implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:4.12")
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts", "additionalScripts/*.gradle.kts")
+        ktlint()
+    }
 }
