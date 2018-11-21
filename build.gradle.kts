a2ebcdc67d4e3beeef947539905baaa85c781d21
Geir André Lund <geir.andre.lund@nav.no>
2018-11-21 14:19:33 +0100

    Restructured Behov object, based on new insight and discussions

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 val avroVersion = "1.8.2"
 group = "no.nav.dagpenger"
-version = "0.1.8-SNAPSHOT"
+version = "0.1.9-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
