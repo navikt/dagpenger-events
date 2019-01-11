1277dd08b4de6f7fd36d31c7b2121158de7b69a1
Geir André Lund <geir.andre.lund@nav.no>
2019-01-11 10:59:53 +0100

    Added logicalType decimal to belop

---
+++ src/test/kotlin/no/nav/dagpenger/events/InntektTest.kt
@@ -0,0 +1,23 @@
+package no.nav.dagpenger.events
+
+import no.nav.dagpenger.events.avro.Inntekt
+import no.nav.dagpenger.events.avro.Maned
+import org.junit.Test
+import java.math.BigDecimal
+import kotlin.test.assertEquals
+
+class InntektTest {
+
+    @Test fun `create inntekt`() {
+        val inntekt = Inntekt.newBuilder().apply {
+            virksomhet = "193984823"
+            belop = BigDecimal(100)
+            ar = 2018
+            maned = Maned.april
+            inntektstype = "Lonnsinntekt"
+            beskrivelse = "Beskrivelse"
+        }.build()
+
+        assertEquals(inntekt.getBelop(), BigDecimal(100))
+    }
+}
