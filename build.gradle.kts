7763056c944da5635acf8371b694a524332d0bac
Giao The Cung <giao.the.cung@nav.no>
2019-04-13 01:44:15 +0200

    Revert "Remove publishing code."
    This reverts commit e3ef9fd635b259dddef18406e80b0b4946c9144a.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
     id("com.commercehub.gradle.plugin.avro") version "0.14.2"
     id("com.palantir.git-version") version "0.11.0"
     id("maven-publish")
+    id("signing")
+    id("io.codearte.nexus-staging") version "0.20.0"
+    id("de.marcphilipp.nexus-publish") version "0.1.1"
 }
 
 apply {
@@ -56,6 +59,73 @@ dependencies {
 val sourcesJar by tasks.registering(Jar::class) {
     classifier = "sources"
     from(sourceSets["main"].allSource)
+}
+
+publishing {
+    publications {
+        create("mavenJava", MavenPublication::class.java) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+
+            pom {
+                name.set("dagpenger-events")
+                description.set("Avro schemas for dagpenger events")
+                url.set("https://github.com/navikt/dagpenger-events")
+                withXml {
+                    asNode().appendNode("packaging", "jar")
+                }
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        name.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        organization.set("NAV (Arbeids- og velferdsdirektoratet) - The Norwegian Labour and Welfare Administration")
+                        organizationUrl.set("https://www.nav.no")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/navikt/dagpenger-events.git")
+                    developerConnection.set("scm:git:git://github.com/navikt/dagpenger-events.git")
+                    url.set("https://github.com/navikt/dagpenger-events")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                username = System.getenv("OSSRH_JIRA_USERNAME")
+                password = System.getenv("OSSRH_JIRA_PASSWORD")
+            }
+            val version = project.version as String
+            url = if (version.endsWith("-SNAPSHOT")) {
+                uri("https://oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
+        }
+    }
+}
+
+ext["signing.gnupg.keyName"] = System.getenv("GPG_KEY_NAME")
+ext["signing.gnupg.passphrase"] = System.getenv("GPG_PASSPHRASE")
+ext["signing.gnupg.useLegacyGpg"] = true
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
+
+nexusStaging {
+    username = System.getenv("OSSRH_JIRA_USERNAME")
+    password = System.getenv("OSSRH_JIRA_PASSWORD")
+    packageGroup = "no.nav"
+    stagingProfileId = "3a10cafa813c47"
 }
 
 spotless {
