163321f893972747934b6416e790796d02cb49e6
Atle Nordland <atle.nordland@nav.no>
2019-07-19 10:16:48 +0200

    Add posteringsbeskrivelse

--- src/main/kotlin/no/nav/dagpenger/events/inntekt/v1/SpesifisertInntekt.kt
+++ src/main/kotlin/no/nav/dagpenger/events/inntekt/v1/SpesifisertInntekt.kt
@@ -1,11 +1,10 @@
 package no.nav.dagpenger.events.inntekt.v1
 
-import com.squareup.moshi.Json
+import de.huxhorn.sulky.ulid.ULID
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.YearMonth
-import de.huxhorn.sulky.ulid.ULID
 
 data class SpesifisertInntekt(
     val inntektId: InntektId,
@@ -55,8 +54,6 @@ data class InntektId(val id: String) {
     }
 }
 
-data class PosteringsType(val id: PosteringsId, val beskrivelse: String)
-
 data class Periode(
     val startDato: LocalDate,
     val sluttDato: LocalDate
@@ -99,137 +96,73 @@ enum class InntektType {
 }
 
 enum class SpesielleInntjeningsforhold {
-    @Json(name = "hyreTilMannskapPaaFiskeSmaahvalfangstOgSelfangstfartoey")
     HYRE_TIL_MANNSKAP_PAA_FISKE_SMAAHVALFANGST_OG_SELFANGSTFARTOEY,
-    @Json(name = "loennVedArbeidsmarkedstiltak")
     LOENN_VED_ARBEIDSMARKEDSTILTAK,
     UNKNOWN
 }
 
 enum class InntektBeskrivelse {
-    @Json(name = "aksjerGrunnfondsbevisTilUnderkurs")
     AKSJER_GRUNNFONDSBEVIS_TIL_UNDERKURS,
-    @Json(name = "annet")
     ANNET,
-    @Json(name = "arbeidsoppholdKost")
     ARBEIDSOPPHOLD_KOST,
-    @Json(name = "arbeidsoppholdLosji")
     ARBEIDSOPPHOLD_LOSJI,
-    @Json(name = "beregnetSkatt")
     BEREGNET_SKATT,
-    @Json(name = "besoeksreiserHjemmetAnnet")
     BESOEKSREISER_HJEMMET_ANNET,
-    @Json(name = "besoeksreiserHjemmetKilometergodtgjoerelseBil")
     BESOEKSREISER_HJEMMET_KILOMETERGODTGJOERELSE_BIL,
-    @Json(name = "betaltUtenlandskSkatt")
     BETALT_UTENLANDSK_SKATT,
-    @Json(name = "bil")
     BIL,
-    @Json(name = "bolig")
     BOLIG,
-    @Json(name = "bonus")
     BONUS,
-    @Json(name = "bonusFraForsvaret")
     BONUS_FRA_FORSVARET,
-    @Json(name = "elektroniskKommunikasjon")
     ELEKTRONISK_KOMMUNIKASJON,
-    @Json(name = "fastBilgodtgjoerelse")
     FAST_BILGODTGJOERELSE,
-    @Json(name = "fastTillegg")
     FAST_TILLEGG,
-    @Json(name = "fastloenn")
     FASTLOENN,
-    @Json(name = "feriepenger")
     FERIEPENGER,
-    @Json(name = "fondForIdrettsutoevere")
     FOND_FOR_IDRETTSUTOEVERE,
-    @Json(name = "foreldrepenger")
     FORELDREPENGER,
-    @Json(name = "helligdagstillegg")
     HELLIGDAGSTILLEGG,
-    @Json(name = "honorarAkkordProsentProvisjon")
     HONORAR_AKKORD_PROSENT_PROVISJON,
-    @Json(name = "hyretillegg")
     HYRETILLEGG,
-    @Json(name = "innbetalingTilUtenlandskPensjonsordning")
     INNBETALING_TIL_UTENLANDSK_PENSJONSORDNING,
-    @Json(name = "kilometergodtgjoerelseBil")
     KILOMETERGODTGJOERELSE_BIL,
-    @Json(name = "kommunalOmsorgsloennOgFosterhjemsgodtgjoerelse")
     KOMMUNAL_OMSORGSLOENN_OG_FOSTERHJEMSGODTGJOERELSE,
-    @Json(name = "kostDager")
     KOST_DAGER,
-    @Json(name = "kostDoegn")
     KOST_DOEGN,
-    @Json(name = "kostbesparelseIHjemmet")
     KOSTBESPARELSE_I_HJEMMET,
-    @Json(name = "losji")
     LOSJI,
-    @Json(name = "ikkeSkattepliktigLoennFraUtenlandskDiplomKonsulStasjon")
     IKKE_SKATTEPLIKTIG_LOENN_FRA_UTENLANDSK_DIPLOM_KONSUL_STASJON,
-    @Json(name = "loennForBarnepassIBarnetsHjem")
     LOENN_FOR_BARNEPASS_I_BARNETS_HJEM,
-    @Json(name = "loennTilPrivatpersonerForArbeidIHjemmet")
     LOENN_TIL_PRIVATPERSONER_FOR_ARBEID_I_HJEMMET,
-    @Json(name = "loennUtbetaltAvVeldedigEllerAllmennyttigInstitusjonEllerOrganisasjon")
     LOENN_UTBETALT_AV_VELDEDIG_ELLER_ALLMENNYTTIG_INSTITUSJON_ELLER_ORGANISASJON,
-    @Json(name = "loennTilVergeFraFylkesmannen")
     LOENN_TIL_VERGE_FRA_FYLKESMANNEN,
-    @Json(name = "opsjoner")
     OPSJONER,
-    @Json(name = "overtidsgodtgjoerelse")
     OVERTIDSGODTGJOERELSE,
-    @Json(name = "reiseAnnet")
     REISE_ANNET,
-    @Json(name = "reiseKost")
     REISE_KOST,
-    @Json(name = "reiseLosji")
     REISE_LOSJI,
-    @Json(name = "rentefordelLaan")
     RENTEFORDEL_LAAN,
-    @Json(name = "skattepliktigDelForsikringer")
     SKATTEPLIKTIG_DEL_FORSIKRINGER,
-    @Json(name = "sluttvederlag")
     SLUTTVEDERLAG,
-    @Json(name = "smusstillegg")
     SMUSSTILLEGG,
-    @Json(name = "stipend")
     STIPEND,
-    @Json(name = "styrehonorarOgGodtgjoerelseVerv")
     STYREHONORAR_OG_GODTGJOERELSE_VERV,
-    @Json(name = "svangerskapspenger")
     SVANGERSKAPSPENGER,
-    @Json(name = "timeloenn")
     TIMELOENN,
-    @Json(name = "trekkILoennForFerie")
     TREKK_I_LOENN_FOR_FERIE,
-    @Json(name = "uregelmessigeTilleggKnyttetTilArbeidetTid")
     UREGELMESSIGE_TILLEGG_KNYTTET_TIL_ARBEIDET_TID,
-    @Json(name = "uregelmessigeTilleggKnyttetTilIkkeArbeidetTid")
     UREGELMESSIGE_TILLEGG_KNYTTET_TIL_IKKE_ARBEIDET_TID,
-    @Json(name = "yrkebilTjenestligbehovKilometer")
     YRKEBIL_TJENESTLIGBEHOV_KILOMETER,
-    @Json(name = "yrkebilTjenestligbehovListepris")
     YRKEBIL_TJENESTLIGBEHOV_LISTEPRIS,
-    @Json(name = "dagpengerVedArbeidsloeshet")
     DAGPENGER_VED_ARBEIDSLOESHET,
-    @Json(name = "dagpengerTilFisker")
     DAGPENGER_TIL_FISKER,
-    @Json(name = "dagpengerTilFiskerSomBareHarHyre")
     DAGPENGER_TIL_FISKER_SOM_BARE_HAR_HYRE,
-    @Json(name = "sykepengerTilFisker")
     SYKEPENGER_TIL_FISKER,
-    @Json(name = "sykepengerTilFiskerSomBareHarHyre")
     SYKEPENGER_TIL_FISKER_SOM_BARE_HAR_HYRE,
-    @Json(name = "lottKunTrygdeavgift")
     LOTT_KUN_TRYGDEAVGIFT,
-    @Json(name = "vederlag")
     VEDERLAG,
-    @Json(name = "sykepenger")
     SYKEPENGER,
-    @Json(name = "tips")
     TIPS,
-    @Json(name = "skattepliktigPersonalrabatt")
     SKATTEPLIKTIG_PERSONALRABATT
 }
 
